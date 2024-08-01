27d9c1cb0bc43ed87b0b36ac2d1386a8eff320b9
fer <fer@fers-iMac.local>
2024-07-31 18:31:55 -0700

    dede

--- src/pages/contacto/Contacto.jsx
+++ src/pages/contacto/Contacto.jsx
@@ -1,7 +1,7 @@
 import './contacto.css'
 
 export default function Contacto() {
-    return (
+    return (<>
       <div className='contactoContainer'>
 
             <h4>ARZOBISPO, OBISPOS</h4>
@@ -12,6 +12,44 @@ export default function Contacto() {
         <img src='https://www.arquidiocesisdehermosillo.org/images/jesus%20jose.png#joomlaImage://local-images/jesus%20jose.png?width=6245&height=8591' />
         <img src='https://www.arquidiocesisdehermosillo.org/images/arquidiocesis/Nogales.jpg' />
       </div>
-    )
+
+{/*
+      <div className='infoContainer'>
+          <div>
+            <h3>Monseñor Ruy Rendón Leal 111</h3>
+             <p>Dr. Paliza y Ocampo, Col. Centenario</p>
+             <p>Hermosillo Sonora Mexico 83260</p>
+             <h5> Telefonos: 6622132138, 6622131327, 6622172339</h5>
+             <h5>Fax: 6622131327 </h5>
+          </div>
+
+            <div>
+            <h3>Monseñor Ruy Rendón Leal 2222</h3>
+             <p>Dr. Paliza y Ocampo, Col. Centenario</p>
+             <p>Hermosillo Sonora Mexico 83260</p>
+             <h5> Telefonos: 6622132138, 6622131327, 6622172339</h5>
+             <h5>Fax: 6622131327 </h5>
+          </div>
+
+            <div>
+            <h3>Monseñor Ruy Rendón Leal   333</h3>
+             <p>Dr. Paliza y Ocampo, Col. Centenario</p>
+             <p>Hermosillo Sonora Mexico 83260</p>
+             <h5> Telefonos: 6622132138, 6622131327, 6622172339</h5>
+             <h5>Fax: 6622131327 </h5>
+          </div>
+
+
+            <div>
+            <h3>Monseñor Ruy Rendón Leal 44444</h3>
+             <p>Dr. Paliza y Ocampo, Col. Centenario</p>
+             <p>Hermosillo Sonora Mexico 83260</p>
+             <h5> Telefonos: 6622132138, 6622131327, 6622172339</h5>
+             <h5>Fax: 6622131327 </h5>
+          </div>
+      </div>*/}
+
+     
+   </> )
   }
   
